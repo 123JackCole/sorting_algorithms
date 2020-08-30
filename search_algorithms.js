181feeb13d33c90b777df15bb51d438a0675fc80
123JackCole <123jack.cole@gmail.com>
2020-08-29 19:37:07 -0700

    added insertion sort

--- search_algorithms.js
+++ search_algorithms.js
@@ -1,5 +1,7 @@
 const testArray = [9, 1, 5, 10, 3, 2, 8, 4, 7, 6];
 
+// helper methods
+
 const compare = (a, b) => {
     if (a < b) {
         return -1;
@@ -12,15 +14,17 @@ const compare = (a, b) => {
 
 const swap = (arr, a, b) => {
     const temp = arr[a];
-    arr[a] = b;
+    arr[a] = arr[b];
     arr[b] = temp;
 }
+
+// algorithms
 
 const bubbleSort = (arr) => {
     const length = arr.length;
 
     for (let i = 0; i < length; i++) {
-        for (let j = 0; j < length; j++) {
+        for (let j = 0; j < length - 1; j++) {
             if (compare(arr[j], arr[j + 1]) === 1) {
                 swap(arr, j, j + 1);
             }
@@ -31,5 +35,22 @@ const bubbleSort = (arr) => {
 
 console.log(bubbleSort(testArray));
 
+const insertionSort = (arr) => {
+    const length = arr.length;
+    let temp, j;
 
+    for (let i = 1; i < length; i++) {
+        temp = arr[i];
+        j = i - 1;
+        
+        while (j >= 0 && arr[i] > temp) {
+            array[j + 1] = array[j];
+            j--;
+        }
 
+        arr[j + 1] = temp;
+    }
+    return arr;
+}
+
+console.log(insertionSort(testArray));
